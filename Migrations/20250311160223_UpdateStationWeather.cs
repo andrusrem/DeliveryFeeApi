d8a4d79df2a89fc5692409e973b2840f436cda2c
andrusrem <andrusremets@gmail.com>
2025-03-11 18:03:13 +0200

    Upload Migrations

---
+++ Migrations/20250311160223_UpdateStationWeather.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeliveryFeeApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateStationWeather : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "WindSpeed",
+                table: "StationWeathers",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "WeatherPhenomenon",
+                table: "StationWeathers",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VmoCode",
+                table: "StationWeathers",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StationName",
+                table: "StationWeathers",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AirTemp",
+                table: "StationWeathers",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "WindSpeed",
+                table: "StationWeathers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "WeatherPhenomenon",
+                table: "StationWeathers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VmoCode",
+                table: "StationWeathers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StationName",
+                table: "StationWeathers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AirTemp",
+                table: "StationWeathers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
