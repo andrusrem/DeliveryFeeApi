5f457856d25c825b6bcf52bb6d825bab8becf920
andrusrem <andrusremets@gmail.com>
2025-03-11 19:48:54 +0200

    Add functions that allow importing weather data from a URL and creating a list of data with only the needed elements

---
+++ Services/IStationWeatherService.cs
@@ -0,0 +1,10 @@
+﻿using DeliveryFeeApi.Data;
+
+namespace DeliveryFeeApi.Services
+{
+    public interface IStationWeatherService
+    {
+        Task<List<StationWeather>> GetWeatherData();
+        List<StationWeather> DeserializeWeatherData(string xml);
+    }
+}
---
+++ Services/StationWeatherService.cs
@@ -0,0 +1,55 @@
+﻿using DeliveryFeeApi.Data;
+using DeliveryFeeApi.Repository;
+using NuGet.Protocol;
+using System.Net.Http;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace DeliveryFeeApi.Services
+{
+    public class StationWeatherService : IStationWeatherService
+    {
+        private readonly IStationWeatherRepository _stationWeatherRepository;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILogger<StationWeatherService> _logger;
+        public StationWeatherService(IStationWeatherRepository stationWeatherRepository, IHttpClientFactory httpClientFactory, ILogger<StationWeatherService> logger)
+        {
+            _stationWeatherRepository = stationWeatherRepository;
+            _httpClientFactory = httpClientFactory;
+            _logger = logger;
+        }
+
+        public async Task<List<StationWeather>> GetWeatherData()
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            
+            string url = "https://www.ilmateenistus.ee/ilma_andmed/xml/observations.php";
+            using (var request = new HttpRequestMessage(HttpMethod.Get, url))
+            using (var response = await httpClient.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var responseData = await response.Content.ReadAsStringAsync();
+                var weatherData = DeserializeWeatherData(responseData);
+                _logger.LogInformation($"Information: {weatherData.ToJson()}");
+                return DeserializeWeatherData(responseData);
+            }
+        }
+
+        public List<StationWeather> DeserializeWeatherData(string xml)
+        {
+            var doc = XDocument.Parse(xml);
+            var stations = doc.Descendants("station").Select(s => new StationWeather
+            {
+                StationName = (string?)s.Element("name")?.Value,
+                VmoCode = int.TryParse((string?)s.Element("wmocode")?.Value, out int vmo) ? vmo : (int?)null,
+                AirTemp = decimal.TryParse((string?)s.Element("airtemperature")?.Value, out decimal temp) ? temp : (decimal?)null,
+                WindSpeed = decimal.TryParse((string?)s.Element("windspeed")?.Value, out decimal speed) ? speed : (decimal?)null,
+                WeatherPhenomenon = (string?)s.Element("phenomenon")?.Value,
+            }).ToList();
+            return stations;
+        }
+        
+
+    }
+}
