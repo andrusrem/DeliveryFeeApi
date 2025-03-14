a1e42ca7bb5f4ac75806531735033b3d3ddd9fc9
andrusrem <andrusremets@gmail.com>
2025-03-14 18:28:52 +0200

    Add a model class for the Delivery Fee endpoint response.

---
+++ Data/ResponseBody.cs
@@ -0,0 +1,22 @@
+﻿namespace DeliveryFeeApi.Data
+{
+    public class ResponseBody
+    {
+        private string? _message;
+        public decimal? Total { get; set; }
+        public bool Forbitten { get; set; } = false;
+        public string? ErrorMessage 
+        { 
+            get {  return _message; }
+            set
+            {
+                if (Forbitten == true)
+                {
+                    value = "Usage of selected vehicle type is forbidden";
+                    _message = value;
+                }
+            }
+        }
+
+    }
+}
